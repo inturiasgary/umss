228e2046f913ffc1f43765eb00812150210fca31
gary <gary@gary-laptop.(none)>
2009-03-04 20:23:08 -0400

    changed admin

--- Proyecto/sistema/admin.py
+++ Proyecto/sistema/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-from models import Developer, Repository, Membership, Message, ToDo, Task, Action
+from models import Developer, Repository, Membership, Message
 
 admin.site.register(Developer)
 admin.site.register(Repository)
 admin.site.register(Membership)
 admin.site.register(Message)
-admin.site.register(ToDo)
-admin.site.register(Task)
-admin.site.register(Action)
-
-
